0b5cc1924a53629b073ff1993fb0fc7a1390680e
Oscar Yang Liu <yqfcpy@hotmail.com>
2021-05-04 00:43:11 +0200

    TODO 每隔多少分钟请求更新ip

---
+++ common/descryptandencrypt.py
@@ -0,0 +1,38 @@
+__author__ = 'Administrator'
+
+import binascii
+
+from pyDes import des, CBC, PAD_PKCS5
+
+
+class PasswordHandle:
+  # 加密
+  @staticmethod
+  def encrypt(s):
+    """
+    DES 加密
+    :param s: 原始字符串
+    :return: 加密后字符串，16进制
+    """
+    secret_key = 'fd+25_*4'  # 密码 要求8位
+    iv = secret_key  # 偏移
+    # secret_key:加密密钥，CBC:加密模式，iv:偏移, padmode:填充
+    des_obj = des(secret_key, CBC, iv, pad=None, padmode=PAD_PKCS5)
+    # 返回为字节
+    secret_bytes = des_obj.encrypt(s, padmode=PAD_PKCS5)
+    # 返回为16进制
+    return binascii.b2a_hex(secret_bytes)
+
+  # 解密
+  @staticmethod
+  def descrypt(s):
+    """
+    DES 解密
+    :param s: 加密后的字符串，16进制
+    :return:  解密后的字符串
+    """
+    secret_key = 'fd+25_*4' # 要求8位
+    iv = secret_key
+    des_obj = des(secret_key, CBC, iv, pad=None, padmode=PAD_PKCS5)
+    decrypt_str = des_obj.decrypt(binascii.a2b_hex(s), padmode=PAD_PKCS5)
+    return str(decrypt_str, encoding='utf8')
---
+++ common/requestools.py
@@ -0,0 +1,56 @@
+__author__ = 'Administrator'
+
+import json
+
+from PyQt5 import QtNetwork
+from PyQt5.QtCore import QObject, pyqtSignal, QUrl, QUrlQuery, QJsonDocument
+
+
+class AsyncRequest(QObject):
+  getResult = pyqtSignal(dict)
+
+  def __init__(self):
+    super().__init__()
+
+  def get(self, url: str, param: dict = None):
+    # 创建一个请求
+    path = QUrl(url)
+    if param != None:
+      query = QUrlQuery()
+      for item in param.items():
+        query.addQueryItem(item[0], item[1])
+      path.setQuery(query.query())
+    req = QtNetwork.QNetworkRequest(path)
+    self.nam = QtNetwork.QNetworkAccessManager()
+    self.nam.finished.connect(self.handleResponse)
+    # 使用get请求 如果有参数的话 写一个data 放到get里
+    self.nam.get(req)
+
+  def post(self, url: str, jsonBody: dict, param: dict = None):
+    path = QUrl(url)
+    sendData = QJsonDocument(jsonBody)
+    if param != None:
+      queryParams = QUrlQuery()
+      for item in param.items():
+        queryParams.addQueryItem(item[0], item[1])
+      path.setQuery(queryParams.query())
+    req = QtNetwork.QNetworkRequest(path)
+    # 设置头信息是json这里可以不写
+    # req.setHeader(QtNetwork.QNetworkRequest.ContentTypeHeader, "application/json")
+    self.nam = QtNetwork.QNetworkAccessManager()
+    self.nam.finished.connect(self.handleResponse)
+    self.nam.post(req, sendData.toJson())
+
+  def handleResponse(self, reply):
+    # replay是发出信号后的返回值
+    er = reply.error()
+    # 如果返回值没有错误的话 执行
+    if er == QtNetwork.QNetworkReply.NoError:
+      bytes_string = reply.readAll()
+      bytes_string_to_json = str(bytes_string, "utf-8")
+      result = {'result': bytes_string_to_json}
+      self.result = result
+    else:
+      errorResult = {'success': False, 'code': 0, 'message': 'Local Exception'}
+      self.result = errorResult
+    self.getResult.emit(self.result)
