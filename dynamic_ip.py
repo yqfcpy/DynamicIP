b8212829b55f3c3caf8f9c9f78651260b1647180
Oscar Yang Liu <yqfcpy@hotmail.com>
2021-05-03 02:32:24 +0200

    托盘

--- dynamic_ip.py
+++ dynamic_ip.py
@@ -2,9 +2,10 @@ __author__ = 'Oscar Yang Liu'
 
 import json
 
-from PyQt5.QtCore import QUrlQuery, QUrl, QTimer, QSettings
+from PyQt5.QtCore import QUrlQuery, QUrl, QTimer, QSettings, QEvent
 from PyQt5.QtGui import QIntValidator, QIcon
-from PyQt5.QtWidgets import QApplication, QWidget, QMessageBox, QSystemTrayIcon, QMenu, QAction, qApp
+from PyQt5.QtNetwork import QLocalSocket, QLocalServer
+from PyQt5.QtWidgets import QApplication, QWidget, QMessageBox, QSystemTrayIcon, QMenu, QAction
 from PyQt5 import QtNetwork
 from resource.UI.main import Ui_main
 from pyDes import des, CBC, PAD_PKCS5
@@ -23,14 +24,11 @@ class DynamicIp(QWidget,Ui_main):
     self.userinfo = self.settings.value('userinfo')
     # 设置文本可以跳转页面
     self.web_lb.setOpenExternalLinks(True)
-
-    if self.userinfo != None:
-      self.domain_le.setText(self.userinfo['domain'])
-      self.username_le.setText(self.userinfo['username'])
-      self.password_le.setText(self.descrypt(self.userinfo['password']))
-      self.update_time_le.setText(self.userinfo['update_time'])
-      self.is_connected(not self.userinfo['editable'])
-
+    # 读取userinfo
+    self.load_userinfo(self.userinfo)
+    # 创建托盘
+    self.create_tuopan()
+    # 获取ip地址
     # https://ipv4.jsonip.com https://ipv6.jsonip.com https://jsonip.com
     self.ip_url = 'https://ipv4.jsonip.com'
     self.get(self.ip_url)
@@ -41,9 +39,11 @@ class DynamicIp(QWidget,Ui_main):
     update_time_validator = QIntValidator(5,1092)
     self.update_time_le.setValidator(update_time_validator)
 
-    # 信号
-    self.request_ip_time.timeout.connect(lambda:print('时间超时'))
 
+    # 信号
+    self.request_ip_time.timeout.connect(lambda: print('时间超时'))
+    # 监控托盘的双击显示和隐藏
+    self.tuopan.activated[self.tuopan.ActivationReason].connect(self.iconActivated)
 
 
   def start_update_ip(self):
@@ -57,11 +57,12 @@ class DynamicIp(QWidget,Ui_main):
     if self.update_time_le.text() == '' or int(self.update_time_le.text())<5 or int(self.update_time_le.text())>1092:
       self.update_time_le.setText('5')
     update_time = self.update_time_le.text()
-    if domain !='' and username !='' and password !='' and update_time !='':
+    if domain != '' and username != '' and password != '' and update_time != '':
       # 密码加密保存到注册表
       encrypt_password = self.encrypt(password)
       self.is_connected(True)
-      self.userinfo = {'domain':domain, 'username':username, 'password':encrypt_password, 'update_time':update_time, 'editable':self.ok_btn.isEnabled()}
+      self.userinfo = {'domain': domain, 'username': username, 'password': encrypt_password,
+                       'update_time': update_time, 'editable': self.ok_btn.isEnabled()}
       self.settings.setValue('userinfo', self.userinfo)
     # 发送更新请求
     try:
@@ -72,21 +73,59 @@ class DynamicIp(QWidget,Ui_main):
 
 
   def edit_info(self):
-    print('change被点击了')
     self.is_connected(False)
     self.userinfo['editable'] = True
-    self.settings.setValue('userinfo',self.userinfo)
+    self.settings.setValue('userinfo', self.userinfo)
 
 
-  def is_connected(self,state:bool):
+  def is_connected(self, state:bool):
     self.domain_le.setDisabled(state)
     self.username_le.setDisabled(state)
     self.password_le.setDisabled(state)
     self.update_time_le.setDisabled(state)
     self.ok_btn.setDisabled(state)
 
-  def is_start_in_window(self,toggle):
-    print('toogle',toggle)
+  def is_start_in_window(self, toggle):
+    print('toogle', toggle)
+    if toggle:
+      # 执行开机自动启动方法
+      pass
+    else:
+      # 不执行开机自动启动方法
+      pass
+
+  # 读取保存的userinfo
+  def load_userinfo(self, userinfo: dict):
+    if userinfo != None:
+      self.domain_le.setText(self.userinfo['domain'])
+      self.username_le.setText(self.userinfo['username'])
+      self.password_le.setText(self.descrypt(self.userinfo['password']))
+      self.update_time_le.setText(self.userinfo['update_time'])
+      self.is_connected(not self.userinfo['editable'])
+    if self.settings.value('autorun') != None:
+      self.window_startup_cb.setChecked(self.settings.value('autorun'))
+
+  # 创建托盘
+  def create_tuopan(self):
+    # 托盘
+    self.tuopan = QSystemTrayIcon(self)  # 创建托盘
+    self.tuopan.setIcon(QIcon(':/img/img/ip.jpg'))  # 设置托盘图标
+    # 创建托盘的右键菜单
+    tpMenu = QMenu()
+    a1 = QAction(u'Show', self, triggered=self.restore_display)
+    a2 = QAction(u'Exit', self, triggered=self.close)
+    tpMenu.addAction(a1)
+    tpMenu.addAction(a2)
+    self.tuopan.setContextMenu(tpMenu)  # 把tpMenu设定为托盘的右键菜单
+    # 设置提示信息
+    self.tuopan.setToolTip(u'Dynamic ip')
+    self.tuopan.show()
+
+  # 托盘中恢复显示的方法
+  def restore_display(self):
+    self.showNormal()
+    self.activateWindow()
+
 
   # get异步请求
   def get(self, url: str, param: dict = None):
@@ -108,14 +147,13 @@ class DynamicIp(QWidget,Ui_main):
     er = reply.error()
     if er == QtNetwork.QNetworkReply.NoError:
       bytes_string = reply.readAll()
-      bytes_to_json = json.loads(str(bytes_string,encoding='utf8'))
+      bytes_to_json = json.loads(str(bytes_string, encoding='utf8'))
       self.ip_lb.setText(bytes_to_json['ip'])
       print(bytes_to_json['ip'])
       # 如果进来的时候已经是enable状态那么直接提交一个更新ip
-      if self.userinfo['editable'] == False:
-        self.update_ip(self.username, self.password, self.domain, bytes_to_json['ip'])
-        self.request_ip_time.start(int(self.update_time_le) * 60 * 1000)
-
+      # if self.userinfo['editable'] == False:
+      #   self.update_ip(self.username, self.password, self.domain, bytes_to_json['ip'])
+      #   self.request_ip_time.start(int(self.update_time_le) * 60 * 1000)
     else:
       self.ip_lb.setText('')
 
@@ -129,7 +167,7 @@ class DynamicIp(QWidget,Ui_main):
     self.get(url, {'hostname': domain, 'myip': new_ip})
 
   # 加密
-  def encrypt(self,s):
+  def encrypt(self, s):
     """
     DES 加密
     :param s: 原始字符串
@@ -145,7 +183,7 @@ class DynamicIp(QWidget,Ui_main):
     return binascii.b2a_hex(secret_bytes)
 
   # 解密
-  def descrypt(self,s):
+  def descrypt(self, s):
     """
     DES 解密
     :param s: 加密后的字符串，16进制
@@ -155,11 +193,20 @@ class DynamicIp(QWidget,Ui_main):
     iv = secret_key
     des_obj = des(secret_key, CBC, iv, pad=None, padmode=PAD_PKCS5)
     decrypt_str = des_obj.decrypt(binascii.a2b_hex(s), padmode=PAD_PKCS5)
-    return str(decrypt_str,encoding='utf8')
+    return str(decrypt_str, encoding='utf8')
+
+  # 最小化 事件
+  def event(self, event):
+    super().event(event)
+    if event.type() == QEvent.WindowStateChange:
+      if self.isMinimized():
+        self.hide()
+    return False
+
 
   # 退出程序保存
   def closeEvent(self, event):
-    reply = QMessageBox.question(self, 'Message', "Are you sure to quit?",
+    reply = QMessageBox.question(self, 'Dynamic ip', "Are you sure to exit?",
                                  QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
     # 判断返回值，如果点击的是Yes按钮，我们就关闭组件和应用，否则就忽略关闭事件
     if reply == QMessageBox.Yes:
@@ -174,15 +221,33 @@ class DynamicIp(QWidget,Ui_main):
       self.userinfo = {'domain': domain, 'username': username, 'password': encrypt_password,
                        'update_time': update_time, 'editable': self.ok_btn.isEnabled()}
       self.settings.setValue('userinfo', self.userinfo)
-
       event.accept()
     else:
       event.ignore()
 
+  # 托盘图标事件
+  def iconActivated(self, reason):
+    if reason == self.tuopan.Trigger:
+      self.showNormal()
+      self.activateWindow()
 
 if __name__ == '__main__':
-  import sys
-  app = QApplication(sys.argv)
-  window = DynamicIp()
-  window.show()
-  sys.exit(app.exec_())
+  # 只允许一个程序启动
+  try:
+    import sys
+    app = QApplication(sys.argv)
+    serverName = 'yqfSoftDDNStestServer'
+    socket = QLocalSocket()
+    socket.connectToServer(serverName)
+    # 如果连接成功，表明server已经存在，当前已有实例在运行
+    if socket.waitForConnected(500):
+      app.quit()
+    else:
+      localServer = QLocalServer()  # 没有实例运行，创建服务器
+      localServer.listen(serverName)
+      # 处理其他事务
+      window = DynamicIp()
+      window.show()
+      sys.exit(app.exec_())
+  except:
+    pass
